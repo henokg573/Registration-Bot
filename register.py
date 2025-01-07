ae8f14175e003d5f09c64b1004cdd5f93e12ddc7
Henok Girma <140075019+henokg573@users.noreply.github.com>
2025-01-07 17:04:03 +0300

    trying....

--- register.py
+++ register.py
@@ -122,7 +122,32 @@ import telebot
 #         print("Shutting down...")
 
 
+import os
+import telebot
+from flask import Flask, request
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+APP_URL = f'https://easygate-registration-bot-34qv.onrender.com/{BOT_TOKEN}'
+
+bot = telebot.TeleBot(BOT_TOKEN)
+app = Flask(__name__)
+
+@app.route(f'/{BOT_TOKEN}', methods=['POST'])
+def webhook():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+@app.route('/')
+def index():
+    return 'Bot is running!', 200
+
+if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=PORT)
 
 
 # Define the bot's handlers
